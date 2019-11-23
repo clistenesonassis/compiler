ea0d6d26c0566237d0c0ffb59f7926c61813e02c
clistenesonassis <clistenes808@gmail.com>
2019-11-23 16:56:16 -0300

    ajustes no léxico para receber float e código junto(sem espaço entre as instruções)

--- service/Lexico.py
+++ service/Lexico.py
@@ -6,7 +6,6 @@ class AnalisadorLexico:
     multiplicativos = ['/', '*']
     chaves = ["program", "var", "integer", "real", "boolean", "procedure", "begin",
               "end", "if", "then", "else", "while", "do", "not", "true", "false"]
-    inteiros = []  # type: List(str)
     delimitadores = [",", ".", ";", ":", "(", ")", "[", "]"]
     relacionais = ["=", "<", ">", "<=", ">=", "<>"]
     atribuicao = [":="]
@@ -19,7 +18,8 @@ class AnalisadorLexico:
                             "Número Inteiro",
                             "Aditivos",
                             "Multiplicativos", 
-                            "Relacional"]
+                            "Relacional",
+                            "Float"]
 
     def __init__(self, arquivoInput):
         self.log = Log()
@@ -28,6 +28,13 @@ class AnalisadorLexico:
     def clearWord(self):
         self.palavra = ""
 
+    def isFloat(self, value):
+        try:
+            float(value)
+        except ValueError:
+            return False
+        return True
+
     def start(self):
         
         for caracter in iter(lambda: self.arquivo.read(1), ''):
@@ -35,53 +42,75 @@ class AnalisadorLexico:
                 while caracter is not '}':
                     caracter = self.arquivo.read(1)
             elif caracter is ' ' or caracter is '\t':
-                self.Classifica()
+                self.separa(self.palavra)
             elif caracter is '\n':
                 self.linhaCont += 1
-                self.Classifica()
+                self.separa(self.palavra)
             else:
                 self.palavra += caracter
-        self.Classifica()
+        self.separa(self.palavra)
 
         self.log.printLog()
 
-    def Classifica(self):
 
-        if self.palavra is not "":
-            self.palavra = self.palavra.lower()
+    def separa(self, word):
+
+        if word is not "":
+            word = word.lower()
             alfabeto = ""
             simbolos = ""
+            ultimoCaracter = ""
+            flagSimbolo = 0
 
-            for caracter in self.palavra:
-                if caracter.isalpha():
-                    alfabeto += caracter
-                else:
-                    if caracter.isdigit() or caracter is '_':
+            for caracter in word:
+                print(caracter)
+                if not flagSimbolo:
+                    if caracter.isalpha():
+                        alfabeto += caracter
+                        ultimoCaracter = caracter
+                    elif (caracter.isdigit() or caracter is '_'):
+                        alfabeto += caracter
+                        ultimoCaracter = caracter
+                    elif alfabeto.isdigit() and caracter is '.':
                         alfabeto += caracter
                     else:
-                        simbolos += caracter
-
-            if alfabeto is not "":
-                if alfabeto in self.chaves:
-                        self.log.addLog( alfabeto, self.tipoDeClassificadores[0], self.linhaCont)
-                elif alfabeto.isdigit():
-                    self.log.addLog( alfabeto, self.tipoDeClassificadores[4], self.linhaCont)
-                else:
-                    self.log.addLog( alfabeto, self.tipoDeClassificadores[1], self.linhaCont)
-
-            if simbolos is not "":
-                if simbolos in self.delimitadores:
-                    self.log.addLog( simbolos, self.tipoDeClassificadores[2], self.linhaCont)
-                elif simbolos in self.atribuicao:
-                    self.log.addLog( simbolos, self.tipoDeClassificadores[3], self.linhaCont)
-                elif simbolos in self.aditivos:
-                    self.log.addLog( simbolos, self.tipoDeClassificadores[5], self.linhaCont)
-                elif simbolos in self.multiplicativos:
-                    self.log.addLog( simbolos, self.tipoDeClassificadores[6], self.linhaCont)
-                elif simbolos in self.relacionais:
-                    self.log.addLog( simbolos, self.tipoDeClassificadores[7], self.linhaCont)
-
+                        self.classifica(alfabeto)
+                        print(alfabeto)
+                        alfabeto = ""
+                        flagSimbolo = 1
+                
+                if flagSimbolo:
+                    simbolos += caracter
+                    if( ultimoCaracter.isalpha() or ultimoCaracter.isdigit() or ultimoCaracter is '_' and len(simbolos) >= 1):
+                        self.classifica(simbolos)
+                        simbolos = ""
+                    flagSimbolo = 0
+            self.classifica(alfabeto)
         self.clearWord()
+
+
+    def classifica(self, word):
+
+        if word is not "":
+            if word in self.chaves:
+                self.log.addLog( word, self.tipoDeClassificadores[0], self.linhaCont)
+            elif word.isdigit():
+                self.log.addLog( word, self.tipoDeClassificadores[4], self.linhaCont)
+            elif word in self.delimitadores:
+                self.log.addLog( word, self.tipoDeClassificadores[2], self.linhaCont)
+            elif word in self.atribuicao:
+                self.log.addLog( word, self.tipoDeClassificadores[3], self.linhaCont)
+            elif word in self.aditivos:
+                self.log.addLog( word, self.tipoDeClassificadores[5], self.linhaCont)
+            elif word in self.multiplicativos:
+                self.log.addLog( word, self.tipoDeClassificadores[6], self.linhaCont)
+            elif word in self.relacionais:
+                self.log.addLog( word, self.tipoDeClassificadores[7], self.linhaCont)
+            elif self.isFloat(word):
+                self.log.addLog( word, self.tipoDeClassificadores[8], self.linhaCont)
+            else:
+                self.log.addLog( word, self.tipoDeClassificadores[1], self.linhaCont)
+
 
 # TO DO
 # -> validar número de ponto flutuante
